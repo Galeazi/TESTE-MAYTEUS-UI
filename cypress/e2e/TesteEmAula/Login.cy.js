28fea9897dd9d0bb05f5b6c07ea4fa93dffa7fb8
Mateus Galeazi <mateusgaleazi01@hotmail.com>
2023-02-01 21:00:52 -0300

    alteração nos testes

--- cypress/e2e/TesteEmAula/Login.cy.js
+++ cypress/e2e/TesteEmAula/Login.cy.js
@@ -2,22 +2,27 @@
 
 context('Funcionalidade Login', ()=>{
 
+beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/')
+});
+
+afterEach(() => {
+    cy.screenshot()
+});
 
     it('Deve fazer login com sucesso', () => {
         
-        cy.visit('http://lojaebac.ebaconline.art.br/')
         cy.get('.icon-user-unfollow').click()
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac (não é aluno_ebac? Sair)')
 
-        
+   
     });
 
     it('Deve exibir uma mensagem de erro ao inserir usuario invalido', () => {
 
-        cy.visit('http://lojaebac.ebaconline.art.br/')
         cy.get('.icon-user-unfollow').click()
         cy.get('#username').type('aluno_ebac@teste')
         cy.get('#password').type('teste@teste.com')
@@ -28,7 +33,6 @@ context('Funcionalidade Login', ()=>{
 
     it('Deve exibir uma mensagem de erro ao inserir senha invalido', () => {
 
-        cy.visit('http://lojaebac.ebaconline.art.br/')
         cy.get('.icon-user-unfollow').click()
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste')

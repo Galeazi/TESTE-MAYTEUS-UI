8c360318e9780169b96ad99cf70f5bc741054205
Mateus Galeazi <mateusgaleazi01@hotmail.com>
2023-02-08 21:09:52 -0300

    validando coom page object

--- cypress/e2e/TesteEmAula/endereco.cy.js
+++ cypress/e2e/TesteEmAula/endereco.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import enderecoPage from "../../support/page-objects/endereco.page";
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -8,9 +9,9 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         })
 
     });
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-        cy.login(dados.usuario, dados.senha)
-
+    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+        enderecoPage.editarEnderecoFaturamento('Pitoco', 'Silvio', 'Petz', 'Brasil', 'Rua luiz scott', '165', 'Barueri', 'São Paulo', '06440260', '11997849556', 'pitoco.silva@teste.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 
 });
